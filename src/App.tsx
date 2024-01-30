9cabd1b871e53153b5b56e2cfec3f638a06dde31
Gustavo Raimo <gustavo.raimo92@gmail.com>
2024-01-29 22:02:32 -0300

    Aplicando as funcionalidades de temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+
 import EstiloGlobal, { Container } from './styles'
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
 import Projetos from './containers/Projetos'
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
 
 function App() {
+  const [useDarkTheme, setUseDarkTheme] = useState(false)
+
+  function trocaTema() {
+    setUseDarkTheme(!useDarkTheme)
+  }
   return (
-    <>
+    <ThemeProvider theme={useDarkTheme ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
