ea245d51e30c084f0cdef3a5c4e41efb3f0693f5
Gustavo Raimo <gustavo.raimo92@gmail.com>
2024-01-29 21:03:25 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/GustavoRaimo.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
 import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/GustavoRaimo.png" alt="" />
-    <Titulo fontSize={20}>Gustavo Raimo</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Gustavo Raimo</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        GustavoRaimo
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Full Stack Python
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
